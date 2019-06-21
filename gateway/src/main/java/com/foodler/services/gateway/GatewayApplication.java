5764647e45d13ab62d9411ad0e82288cce9c3603
Sagnik Bhar <sbhar2@sapient.com>
2019-06-21 13:05:07 +0530

    Introduces Eureka

--- gateway/src/main/java/com/foodler/services/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/foodler/services/gateway/GatewayApplication.java
@@ -3,10 +3,12 @@ package com.foodler.services.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 @EnableOAuth2Sso
 public class GatewayApplication {
 
